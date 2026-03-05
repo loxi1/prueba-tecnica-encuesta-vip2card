bd51904b6fa4fd4f44df3df225bf07b17a0229a5
loxi1 <anibal.cayetano@gmail.com>
2026-03-04 21:18:03 -0500

    change, favicon

--- src/resources/views/layouts/guest.blade.php
+++ src/resources/views/layouts/guest.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ config('app.name', 'VIP2CARS') }}</title>
+  <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-50 text-gray-900">
